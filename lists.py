3200afab40739c2dd01335644a4887f1b39d072f
Andrii Mazur <andreymazur93@gmail.com>
2016-09-25 16:46:43 +0200

    Lists are almost done

---
+++ lists.py
@@ -0,0 +1,198 @@
+#T6Q1
+#A list behaves like a container, and it is able to contain more than one value. Create a list with the
+#values as shown in the example below.
+
+aList = ['Hello', 0, 20.0, 'World']
+
+#T6Q2
+#A list can be modified, and more elements can be added to an existing list. Use the append(x) function
+#to add some more items to the list to produce the same content shown in the sample below.
+
+aList = ['Hello', 0] 
+aList.append(20.0)
+aList.append('World')
+
+#T6Q3
+#A list can be modified, and elements can be removed from an existing list. Use the remove(x) function
+#to remove some items from the list shown in the sample given below so that the list is left with the
+#following content: ['hello', 'python','programming'].
+
+aList = ['hello', 'i', 'love', 'python', 'programming']
+aList.remove('i')
+aList.remove('love')
+
+#T6Q4
+#Write a function addNumbersInList(numbers) to add all the numbers in a list. To access each element in
+#a list, you can use the statement 'for num in numbers:'.
+
+def addNumbersInList(numbers):
+    return sum(i for i in numbers)
+
+#T6Q5
+#Write a function addOddNumbers(numbers) to add all the odd numbers in a list. To access each element in
+#a list, you can use the statement 'for num in numbers:'. 
+
+def addOddNumbers(numbers):
+    return sum(i for i in numbers if i%2 == 1)
+
+#T6Q6
+#Write a function countOddNumbers(numbers) to count the number of odd numbers in a list.
+
+def countOddNumbers(numbers):
+    return (i for i in numbers if i%2 == 1)
+
+#T6Q7
+#Write a function getEvenNumbers(numbers) to return all the even numbers in a list. 
+
+def getEvenNumbers(numbers): 
+    new = []
+    for i in numbers:
+        even = i%2
+        if(even == 0):
+            new.append(i)
+    return new
+
+#T6Q8
+#Write a function removeFirstAndLast(list) that takes in a list as an argument and remove the first and last
+#elements from the list. The function will return a list with the remaining items. 
+
+def removeFirstAndLast(numbers):
+    new = list(numbers)
+    new.pop(0)
+    if not new:
+        return new
+    new.pop()
+    return new
+
+#T6Q9
+#Write a function getMaxNumber(numbers) that returns the maximum number in a list. 
+
+def getMaxNumber(numbers):
+        if(numbers == []):
+            return "N.A"
+        a = list(numbers)
+        return max(a)
+
+#T6Q10
+#Write a function getMinNumber(numbers) that returns the minimum number in a list. 
+
+def getMinNumber(numbers): 
+        if(numbers == []):
+            return "N.A"
+        a = list(numbers)
+        return min(a)
+
+#T6Q11
+#Write a function that does matrix multiplication.
+#The product of a mxn matrix with a nxp matrix results in a mxp matrix.
+#A mxn matrix, with m rows and n columns, can be represented using nested lists.
+#Am,n = [ [x11, x12, ..., x1n], ..., [xm1, ..., xmn] ] 
+
+def MatrixProduct(m1, m2): 
+    s=0     #sum
+    t=[]    #temporary
+    m3=[]   #final matrix
+    if len(m2)!=len(m1[0]):
+        print("Matrices cannot be multiplicated")        
+    else:
+        r1=len(m1)      #number of rows in the first matrix
+        c1=len(m1[0])   #number of columns in the first matrix   
+        r2=c1           #number of rows in the second matrix
+        c2=len(m2[0])   #number of columns in the second matrix
+        for z in range(0,r1):
+            for j in range(0,c2):
+                for i in range(0,c1):
+                   s=s+m1[z][i]*m2[i][j]
+                t.append(s)
+                s=0
+            m3.append(t)
+            t=[]           
+    return m3
+
+#T6Q13
+#A mxn matrix, m rows and n columns, can be represented using
+#nested lists. Write a function that returns the diminensions of a matrix. 
+
+def matrixDimensions(m):
+    column_list = []
+    a = len(m)              # number of rows
+    b = len(m[0])           # number of columns in the first row
+    for i in range(0, a):
+        if(b != len(m[i])):
+           return 'This is not a valid matrix.'
+    return "This is a %dx%d matrix." % (a, b)
+
+#T6Q16
+#Write a function combine(la, lb) that takes in two lists and return
+#a list with the contents of both list sorted in ascending order.
+
+def combine(la, lb):
+    result_list = []
+    result_list.extend(la)
+    result_list.extend(lb)
+    result_list.sort()
+    return result_list
+
+#T6Q17
+#The transpose of a matrix M, denoted MT, is formed by interchanging
+#the rows and columns of M. That is, a mxn matrix is transformed into
+#a nxm matrix.[MT]ij = [M]ji. Write a function that returns the
+#transpose of a matrix. 
+
+#T6Q20
+#Write a function combineList(list1, list2) that takes in two lists
+#as arguments and return a list that combines all the elements in
+#the two list. 
+
+def combineList(list1, list2):
+    res_list = []
+    res_list.extend(list1)
+    res_list.extend(list2)
+    return res_list
+
+#T6Q21
+#Write a function (list1, list2) that takes in two lists as arguments
+#and return a list that is the result of removing elements from list1
+#that can be found in list2. 
+
+def subtractList(list1, list2):
+    new_list = []
+    for i in (list1):
+        if i not in list2:
+           new_list.append(i)
+    return new_list
+
+#T6Q23
+#Write a function getNumbers(number) that takes in a number as argument
+#and return a list of numbers as shown in the samples given below
+#>>> getNumbers(10)
+#    [100, 64, 36, 16, 4, 0, 4, 16, 36, 64, 100]
+
+def getNumbers(num): 
+    final_list = []
+    for i in range(-num,num+1,2):
+        final_list.append(i*i)
+    return final_list
+
+#T6Q24
+#Write a function getSumOfFirstDigit(numList) that takes in a list of
+#positive numbers and returns the sum of all the first digit in the list. 
+
+
+
+#T6Q26
+#List comprehension offers a concise way to derive a new list from an
+#existing list or sequence. Given a list of numbers, write a function
+#that returns the numbers that are greater than the average. 
+
+def getAboveAverage(nums):
+    return [ x for x in nums if x > (sum(nums)/float(len(nums))) ]
+
+
+
+
+
+
+
+
+ 
---
+++ while.py
@@ -0,0 +1,156 @@
+#T5Q1
+#Create a function addNumbers(x) that takes a number as an argument and adds all the integers between 1
+#and the number (inclusive) and returns the total number.
+
+def addNumbers(num):
+    total = 0
+    i = 1
+    while i<=num:
+        total+=i
+        i+=1
+    return total
+
+#T5Q2
+#Create a function addNumbers(start, end) that adds all the integers between the start and end value (inclusive)
+#and returns the total sum. 
+
+def addNumbers(start, end):
+    total = 0
+    while start <= end :
+        total += start
+        start += 1
+    return total
+
+#T5Q4
+#Create a function countPages(x) that takes the number of pages of a book as an argument and counts the number of
+#times the digit '1' appears in the page number.
+
+def countPages(num):
+    total = 0
+    i = 1
+    while i<=num:
+        page_no = str(i)
+        total += page_no.count('1')
+        i+=1 
+    return total 
+
+#T5Q5
+#Create a function factorial(x) that takes an integer and returns the product of all the positive integers less than
+#or equal to n. 
+
+def factorial(num):
+    product = 1
+    i = 1
+    while i <= num:
+        product *=i
+        i+=1
+    return product 
+
+#T5Q6
+#Create a function doubleFactorial(n) that takes an odd integer and returns the product of all odd values up to the
+#value n (where n=2k-1). 
+
+def doubleFactorial(num):
+    product = 1 
+    i = 0
+    k = 0 
+    while k < num:
+       k = 2*i+1
+       product *= k 
+       i += 1
+    return product
+
+#T5Q7
+#Create a function that takes in a positive integer and return a list of prime numbers.
+#A prime number is only divisible by 1 and itself. 
+
+def primeNumbers(num):
+    primes = []
+    i = 2
+    # iterates through range from 2 to num(inclusive)
+    while i <= num     :     # add 'while' condition
+        k = 2
+        isPrime = True
+        # check if prime number
+        while k != i :      # add 'while' condition
+            if i%k==0:
+                isPrime = False
+            k+=1            # update k
+        if isPrime:
+            primes.append(i)
+        i+=1              # update i
+    return primes
+
+#T5Q9
+#Create a function that takes in a positive number and return 2 integers such that the number is
+#between the squares of the 2 integers. It returns the same integer twice if the number is a square
+#of an integer.
+
+from math import floor
+
+def sqApprox(num):
+    fnum = int(floor(num))
+    i = 0
+    while True:
+        if i * i <= fnum:
+            minsq = i
+        if i * i >= num:
+            maxsq = i
+            break
+        i = i + 1
+
+    return minsq, maxsq
+
+#T5Q10
+#Create a function that computes the approximation of pi, based on the number of iterations specified. 
+
+def piApprox(num):
+    i = 1
+    pi = 0
+    k = 1
+    while i <= num     :                # set 'while' termination condition
+        if (i % 2 == 0):
+            pi = pi - 1. / float(k)
+        else:
+            pi = pi + 1. / k
+        k+=2                      # compute the ith term of the series
+        i+=1                       # update i
+    return 4.0*float(pi)
+
+#T5Q11
+#Write a function estimatePi() to estimate and return the value of pi based on the formula found
+#by an Indian Mathematician Srinivasa Ramanujan. It should use a while loop to compute the terms
+#of the summation until the last item is smaller than 1e -15.
+
+import math
+
+def series_term(k):
+    a=factorial(4*k)
+    b=(1103+26390*k)
+    c=factorial(k)
+    d=c**4
+    e=396**(4*k)
+    return float(a*b)/(d*e)
+
+def estimatePi():
+    k=0
+    final=0
+    while True:
+        term = series_term(k)
+        final += term
+        if term < 1.0e-15:
+            break
+        else:
+            k += 1
+    f=2*math.sqrt(2)/9801 # compute this only once
+    pi = 1.0/(final * f)
+    return pi
+
+#T5Q12
+#Given a positive integer, write a function that computes the prime factors that can be multplied
+#together to get back the same integer. 
+
+#T5Q13
+#The smallest common multiple of two or more numbers is called the lowest common multiple (LCM).
+#Given a list of integers, find the lowest common multiple. 
+    
